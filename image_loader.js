96a646097d92bf11ca7075d2f19729931a6fa382
C33purr <C33purr@gmail.com>
2026-09-12 17:14:05 +0200

    Code commit

---
+++ image_loader.js
@@ -0,0 +1,11 @@
+chrome.tabs.query(
+	{active: true, currentWindow: true}
+	, function(tabs) {
+	  chrome.tabs.sendMessage(tabs[0].id, {
+		  command: "init", 
+		  function(response) {
+			console.log(response.result);
+		  }
+		});
+	}
+);
